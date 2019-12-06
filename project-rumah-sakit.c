f12462442841cb77fdc0bda5046ce55119d2ae6f
YunusIsmail <yunusismail99@gmail.com>
2019-12-06 09:43:32 +0700

    Input,View,Update

--- project-rumah-sakit.c
+++ project-rumah-sakit.c
@@ -21,42 +21,89 @@ struct pasien {
 };
 typedef struct pasien data_pasien;
 
-int input(){
-	int i;
-	i=0;
-	
-	data_pasien data[i];
-	
-	printf("Masukan Nama Pasien : ");
-	scanf("%s", &data[i].nama_pasien); fflush(stdin);
-	printf("Masukan Jenis Penyakit Pasien : ");
-	scanf("%s", &data[i].jenis_penyakit); fflush(stdin);
+data_pasien data[50];
+
+int i=0;
+
+void input(int i){	
+	printf("Masukan ID Pasien : ");
+	scanf("%d", &data[i].id_pasien);
+	printf("Masukan Nama Pasien : "); fflush(stdin);
+	gets(data[i].nama_pasien); 
+	printf("Masukan Jenis Penyakit Pasien : "); fflush(stdin);
+	gets(data[i].jenis_penyakit); 
 	printf("Masukan Jenis Kamar : ");
 	scanf("%s", &data[i].kamar.jenis_kamar);
 	printf("Masukan Nomor Kamar : ");
 	scanf("%d", &data[i].kamar.nomor_kamar);
 	
-	i=i+1;
-	printf("%d", i);
+	system("pause");
+}
+
+void view(int i){
+	int k;
+	for(k=0; k<i; k++){
+		printf("ID Pasien : %d\n", data[k].id_pasien);
+		printf("Nama Pasien : %s\n", data[k].nama_pasien);
+		printf("Jenis Penyakit : %s\n", data[k].jenis_penyakit);
+		printf("Jenis Kamar : %s\n", data[k].kamar.jenis_kamar);
+		printf("Nomor Kamar : %d\n\n", data[k].kamar.nomor_kamar);
+	}
+}
+
+void update(int i){
+	int id_update, k;
+	printf("Masukan ID Pasien : ");
+	scanf("%d", &id_update);
+	
+	for(k=0; k<i; k++){
+		if(id_update == data[k].id_pasien){
+			printf("ID Pasien : %d\n", data[k].id_pasien);
+			printf("Masukan Nama Pasien : "); fflush(stdin);
+			gets(data[k].nama_pasien); 
+			printf("Masukan Jenis Penyakit Pasien : "); fflush(stdin);
+			gets(data[k].jenis_penyakit); 
+			printf("Masukan Jenis Kamar : ");
+			scanf("%s", &data[k].kamar.jenis_kamar);
+			printf("Masukan Nomor Kamar : ");
+			scanf("%d", &data[k].kamar.nomor_kamar);
+		}
+	}
+}
+
+void menu_utama(){
+	int pilih, i;
+	
+	do{
+		printf("===== SISTEM INFORMASI RUMAH SAKIT =====\n");
+		printf("1. Daftarkan Pasien\n");
+		printf("2. Lihat Data Pasien\n");
+		printf("3. Update Data Pasien\n");
+		printf("4. Cari Data\n");
+		printf("5. Urutkan Data\n");
+		printf("Masukan Pilihan Anda : ");
+		scanf("%d", &pilih);
+	
+		if(pilih == 1){
+			system("cls");
+			input(i);
+			i+=1;
+			system("cls");
+		} else if(pilih == 2){
+			system("cls");
+			view(i);
+			system("pause");
+			system("cls");
+		} else if(pilih == 3){
+			system("cls");
+			update(i);
+			system("pause");
+			system("cls");
+		}
+	} while(pilih != 1 || pilih != 2 || pilih != 3 || pilih != 4 || pilih != 5);
 }
 
 int main() {
-	int pilih;
-	
-	printf("===== SISTEM INFORMASI RUMAH SAKIT =====\n");
-	printf("1. Daftarkan Pasien\n");
-	printf("2. Lihat Data Pasien\n");
-	printf("3. Update Data Pasien\n");
-	printf("4. Cari Data\n");
-	printf("5. Urutkan Data\n");
-	printf("Masukan Pilihan Anda : ");
-	scanf("%d", &pilih);
-	
-	if(pilih == 1){
-		input();
-	}else{
-		return 0;
-	}
-	
+	menu_utama();
 	return 0;
 }
