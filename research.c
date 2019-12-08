7447a9480bdabe4c607bf798be3022443419256d
Dimas Rehan Nabiel <dimasrehannabiel@gmail.com>
2019-12-08 09:22:11 +0700

    Beberapa penggantian tipe data pada struct

--- research.c
+++ research.c
@@ -6,7 +6,14 @@ int data[] = {4,7,1,2,9,5,11,21,15,10,3,42,12,53,20,18,60,1};
 //Kamar pasien
 struct kamar 
 {
-	char jenis_kamar[10];
+    /*
+        Jenis Kamar : 
+            1.VIP
+            2.III
+            3.II
+            4.I
+    */
+	int jenis_kamar;
 	int nomor_kamar;
 };
 
@@ -23,45 +30,51 @@ struct pasien
 {
 	int id_pasien;
 	char nama_pasien[50];
-	char jenis_penyakit[20];
+	int jenis_penyakit;
+    /*
+        Jenis Penyakit : 
+            1. Demam Berdarah
+            2. Kecelakaan
+            3. Lain-lain
+    */
 };
 
 //Data seluruh struct menjadi satu kesatuan
 struct rumah_sakit
 {
-    struct kamar kamar;
-    struct masuk masuk;
     struct pasien pasien;
+    struct masuk masuk;
+    struct kamar kamar;
 };
 typedef rumah_sakit;
 
-// merging_char(int low, int mid, int high) 
-// {  
-//     int temp[high + 1];
-//     int i = low;
-//     int j = mid + 1;
-//     int k = 0;
+merging(int low, int mid, int high) 
+{  
+    int temp[high + 1];
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
 
-//     while (i <= mid && j <= high)
-//     {
-//         if (data[i] <= data[j])
-//             temp[k++] = data[i++];
-//         else
-//             temp[k++] = data[j++];
-//     }
-//     while (i <= mid)
-//         temp[k++] = data[i++];
-//     while (j <= high)
-//         temp[k++] = data[j++];
-//     k--;
-//     while (k >= 0)
-//     {
-//         data[k + low] = temp[k];
-//         k--;
-//     }
-// }
+    while (i <= mid && j <= high)
+    {
+        if (data[i] <= data[j])
+            temp[k++] = data[i++];
+        else
+            temp[k++] = data[j++];
+    }
+    while (i <= mid)
+        temp[k++] = data[i++];
+    while (j <= high)
+        temp[k++] = data[j++];
+    k--;
+    while (k >= 0)
+    {
+        data[k + low] = temp[k];
+        k--;
+    }
+}
 
-sort_char(rumah_sakit data, int low, int high) 
+void sort(int low, int high) 
 {
     int mid;
     
@@ -69,7 +82,7 @@ sort_char(rumah_sakit data, int low, int high)
         mid = (low + high) / 2;
         sort(low, mid);
         sort(mid+1, high);
-        merging_char(low, mid, high);
+        merging(low, mid, high);
     } else { 
         return;
     }   
@@ -77,30 +90,31 @@ sort_char(rumah_sakit data, int low, int high)
 
 main()
 {
-    struct rumah_sakit data[5];
+    struct rumah_sakit data_sakit[3];
     int banyak_string, banyak_data;
     int a,b;
 
-    strcpy(data[0].kamar.jenis_kamar, "REG");
-    strcpy(data[1].kamar.jenis_kamar, "VIP");
+    data_sakit[0].pasien.id_pasien = 0;
+    strcpy(data_sakit[0].pasien.nama_pasien, "Rehan");
+    strcpy(data_sakit[0].kamar.jenis_kamar, "REG");
+    
 
-    banyak_data = sizeof(data) / sizeof(data[0]);
-    banyak_string = sizeof(data[0].kamar.jenis_kamar) / sizeof(data[0].kamar.jenis_kamar[0]);
+    strcpy(data_sakit[1].kamar.jenis_kamar, "VIP");
+
+    banyak_data = sizeof(data_sakit) / sizeof(data_sakit[0]);
+    banyak_string = sizeof(data_sakit[0].kamar.jenis_kamar) / sizeof(data_sakit[0].kamar.jenis_kamar);
     
     printf("%d \n", banyak_string);
     printf("%d \n", banyak_data);
 
     printf("Data asli : \n");
     for(a = 0; a < banyak_data; a++)
-    {
-        printf("[%s] ", data[a].kamar.jenis_kamar);
-    }
+        printf("[%d] ", data[a]);
 
-    sort(0);
-    // sort(0, banyak_data);
-    // printf("\n\n");
+    sort(0, banyak_data);
+    printf("\n\n");
 
-    // printf("Data setelah Merge sort :  \n");
-    // for(a = 0; a < banyak_data; a++)
-    // printf("[%d] ", data[a]);
+    printf("Data setelah Merge sort :  \n");
+    for(a = 0; a < banyak_data; a++)
+        printf("[%d] ", data[a]);
 }
